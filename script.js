223a7a5a7b8a37038812ef0e93e9921d06d465fb
Francisco Salazar <fran.pacorro2001@gmail.com>
2021-12-02 16:36:37 -0600

    a

--- script.js
+++ script.js
@@ -65,89 +65,10 @@ video.addEventListener('play', async () => {
       drawBox.draw(canvas)
     })
 
-
-    /*
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    */
-
   }, 100)
 
 })
 
-/*
-async function start() {
-
-  // crea un div para poner la imagen
-  const container = document.createElement('div')
-  container.style.position = 'relative'
-  document.body.append(container)
-
-  // llama la cosa que pone los nombres
-  const labeledFaceDescriptors = await loadLabeledImages()
-
-  // Matcher para la cara con las etiquetas para ver si encuentra algo
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-
-  let image
-  let canvas
-
-  document.body.append('Loaded')
-
-  // listener para sincronizar el matcher y la imagen load
-  imageUpload.addEventListener('change', async () => {
-
-    // si hay una imagen la quita
-    if (image) image.remove()
-    if (canvas) canvas.remove()
-
-    // convierte el archivo en imagen
-    image = await faceapi.bufferToImage(imageUpload.files[0])
-
-    // muestra la image en el div
-    container.append(image)
-
-    // crea el canvas donde va a dibujar las cajas
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-
-    // ajusta el tamaño
-    image.width = 500
-    image.height = 500
-    const displaySize = { width: image.width, height: image.height }
-
-    // ajusta el tamaño del canvas con el tamaño de la imagen
-    faceapi.matchDimensions(canvas, displaySize)
-
-    // Variable de las cajas de detección de caras
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-
-    // escala las detecciones para que concuerden con la imagen
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-
-    // Variable donde almacena la cara detectada con el nombre
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-
-    // forEach de los resultados que encontró en la foto dibujando cada caja con su nombre
-    results.forEach((result, i) => {
-      // caja
-      const box = resizedDetections[i].detection.box
-      // dibujo de caja que concuerde con la detección
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-
-      // lo pinta
-      drawBox.draw(canvas)
-    })
-
-
-  })
-
-}*/
 
 function loadLabeledImages() {
 
